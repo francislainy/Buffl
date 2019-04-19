9cec6634d5c4a4772ae1be7906c5cad62fe04f70
Francislainy <francislainy.campos@gmail.com>
2019-04-19 17:44:36 +0100

    Add sign out and redirect back to login

--- app/src/main/java/com/francislainy/buffl/activities/MainActivity.kt
+++ app/src/main/java/com/francislainy/buffl/activities/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.francislainy.buffl.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
 import com.francislainy.buffl.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvLogout.setOnClickListener {
+
+            // FirebaseAuth.getInstance().signOut()
+
+            AuthUI.getInstance().signOut(this)
+                .addOnCompleteListener {
+                    // user is now signed out
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                }
+
+        }
     }
 
 }
