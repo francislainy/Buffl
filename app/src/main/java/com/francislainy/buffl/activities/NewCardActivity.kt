1867f0ea4135a4a9d2f664b1c968a2b9e98b7586
Francislainy <francislainy.campos@gmail.com>
2019-05-01 17:46:36 +0100

    Start working on NewCardActivity and their fragment - Layout nearly done

--- app/src/main/java/com/francislainy/buffl/activities/NewCardActivity.kt
+++ app/src/main/java/com/francislainy/buffl/activities/NewCardActivity.kt
@@ -1,6 +1,5 @@
 package com.francislainy.buffl.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -8,43 +7,38 @@ import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import com.francislainy.buffl.R
 import com.francislainy.buffl.fragments.CoursePagerControllerFragment
-import com.francislainy.buffl.model.Course
+import com.francislainy.buffl.fragments.NewCardFragment
 import com.francislainy.buffl.utils.ToolbarAndNavController
 import com.francislainy.buffl.utils.addFragment
-import com.francislainy.buffl.utils.objectFromJsonString
 import kotlinx.android.synthetic.main.activity_main.*
 
-class LearnExploreActivity : AppCompatActivity() {
-
-    private var objectTitle: String? = null
+class NewCardActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
 
-        displayToolbar(2, objectTitle!!) //todo: have dynamic position - 21/04/19
+        displayToolbar(param = "Create Card") //todo: have dynamic position - 21/04/19
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_learn_explore)
+        setContentView(R.layout.activity_new_card)
 
+        invalidateOptionsMenu()
         toolbarActionBarSetUP()
 
-        val objectString = intent.getStringExtra("objectString")
-        objectTitle = objectFromJsonString(objectString, Course::class.java).courseTitle
-
-        addFragment(CoursePagerControllerFragment.newInstance(objectString!!), R.id.container_body_learn)
+        addFragment(NewCardFragment(), R.id.container_body_new_card)
     }
 
-    private fun displayToolbar(pos: Int, param: String) {
-        ToolbarAndNavController(this@LearnExploreActivity).toolbarSetUP(pos, param)
+    private fun displayToolbar(pos: Int = 0, param: String) {
+        ToolbarAndNavController(this).toolbarSetUP(pos, param)
     }
 
     private fun toolbarActionBarSetUP() {
         setSupportActionBar(toolbar as Toolbar)
         supportActionBar?.apply {
             setDisplayShowHomeEnabled(false)
-            setDisplayHomeAsUpEnabled(false)
+            setDisplayHomeAsUpEnabled(true)
             title = null
         }
     }
@@ -52,14 +46,13 @@ class LearnExploreActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main_menu, menu)
-
+        menu.findItem(R.id.menu_settings).isVisible = false
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.menu_plus -> {
-                displayNewCardActivity()
                 return true
             }
             R.id.menu_settings -> {
@@ -68,11 +61,6 @@ class LearnExploreActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
-    }
-
-    private fun displayNewCardActivity() {
-        val intent = Intent(this, NewCardActivity::class.java)
-        startActivity(intent)
     }
 
 }
